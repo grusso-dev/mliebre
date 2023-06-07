32e47cb24e741d9fe68020cbad77ca2201d063dd
Gustavo Russo <russo.gustavo@gmail.com>
2023-06-06 23:00:19 -0300

    Modificacion de los estilos y agregue las nuevas vistas

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,"/views/index.html"))
 })
 
+app.get('/login/', (req, res) => {
+  res.sendFile(path.join(__dirname,"/views/login.html"))
+})
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname,"/views/register.html"))
+})
+
 const port = 3000
 app.listen(port, () => {
   console.log(`Aplicacion escuchando en el puerto: ${port}`)
