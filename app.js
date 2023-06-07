ea9ce13011307aa3880af044122afe7c634c8c9a
Gustavo Russo <russo.gustavo@gmail.com>
2023-06-07 18:13:41 -0300

    Agregue unas pruebas para ariel

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-app.use(express.static(path.join(__dirname,"/public")))
+app.use(express.static(path.join(__dirname, "/public")))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,"/views/index.html"))
+  res.sendFile(path.join(__dirname, "/views/index.html"))
+})
+app.get('/prueba', (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/index2.html"))
 })
 
 app.get('/login/', (req, res) => {
-  res.sendFile(path.join(__dirname,"/views/login.html"))
+  res.sendFile(path.join(__dirname, "/views/login.html"))
 })
 
 app.get('/register', (req, res) => {
-  res.sendFile(path.join(__dirname,"/views/register.html"))
+  res.sendFile(path.join(__dirname, "/views/register.html"))
 })
 
 const port = 3000
